b32dd968d2fd13d495a8b2da13ba8826167dce3d
Juan de Leon <jdele185@stu.southtexascollege.edu>
2023-05-09 14:51:42 -0500

    Graphics

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-const title = ref('Desired Route ✋')
+const title = ref('Desired Route 🗺️')
 </script>
 
 <template>
--- src/views/OtherPage.vue
+++ src/views/OtherPage.vue
@@ -1,5 +1,5 @@
 <template>
   <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">Available Deliveries</h1>
+    <h1 class="text-6xl font-thin text-slate-800">Available Deliveries 🚚</h1>
   </main>
 </template>
