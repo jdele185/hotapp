50a20b5b38f9bb46c928f12f42131f9893b7415d
Juan de Leon <jdele185@stu.southtexascollege.edu>
2023-05-09 16:09:36 -0500

    Buttons

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,5 +6,10 @@ const title = ref('Desired Route 🗺️')
 <template>
   <main class="flex min-h-screen items-center justify-center">
     <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
+    <form class="destination-form" @submit="destinations">
+      <input type="text" placeholder="Destination" />
+      <button type="submit" class="bg-blue-400 px-2 py-2" @click="destinations">Destination</button>
+    </form>
   </main>
 </template>
+
--- src/views/OtherPage.vue
+++ src/views/OtherPage.vue
@@ -1,5 +1,9 @@
 <template>
   <main class="flex min-h-screen items-center justify-center">
     <h1 class="text-6xl font-thin text-slate-800">Available Deliveries 🚚</h1>
+    <form class="delivery-form" @submit="deliveries">
+      <input type="text" placeholder="Delivery" />
+      <button type="submit" class="bg-yellow-400 px-2 py-2" @click="deliveries">Deliveries</button>
+    </form>
   </main>
 </template>
